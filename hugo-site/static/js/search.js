6aa558b85e394d4ea843aadb7728b58748ae9ac3
Jerad Shoemaker <jshoemakerhwc@gmail.com>
2026-07-22 15:59:48 -0700

    Add Medications handout section + search index

--- hugo-site/static/js/search.js
+++ hugo-site/static/js/search.js
@@ -7,7 +7,7 @@
     var blogContentIndex = null;  // will be an array once loaded
 
     // Load the pre-built blog content index (cache-busted so newly-added posts appear immediately)
-    fetch('js/blog-index.json?v=20260719a')
+    fetch('js/blog-index.json?v=20260722b')
       .then(function(r) { return r.ok ? r.json() : []; })
       .then(function(data) { blogContentIndex = data; })
       .catch(function() { blogContentIndex = []; });
